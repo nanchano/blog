e994d5efab4845cc45102fb243d76d25ae0c1fa8
nanchano <nicolas.anchanio@gmail.com>
2023-09-28 13:11:47 -0300

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,30 @@
-<h1>About</h1>
-<p>TBD</p>
+<svelte:head>
+	<title>About Me</title>
+</svelte:head>
+
+<div class="container">
+	<h1>About Me</h1>
+	<div class="content">
+		<p>
+			I'm a Software and Data Engineer who enjoys building quality
+			services end to end, from infrastructure and backend to testing and
+			deployment.
+		</p>
+		<br />
+		<p>
+			Passionate about free software, clean code, design patterns and
+			DevOps culture.
+		</p>
+		<br />
+		<p>
+			Looking to write about free and open source tools for development,
+			as well as software architecture and design from a day to day
+			working perspective.
+		</p>
+		<br />
+		<p>
+			Things I've enjoyed these past few years: Neovim, Python, Go,
+			Terraform, Kubernetes and a myriad of GCP services.
+		</p>
+	</div>
+</div>
