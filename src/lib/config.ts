6e6d67d8385ee81fcb5c92b35dfa14e247cda0fc
nanchano <nicolas.anchanio@gmail.com>
2023-09-28 15:27:16 -0300

    better description

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import {dev} from '$app/environment';
 
 export const title = 'Nicolas Anchano';
-export const description = 'My personal blog';
+export const description = 'My blog about Software and Engineering, free and open source software tools, and related.';
 export const url = dev ? 'http://localhost:5173/': 'https://tiburcio.vercel.app/';
