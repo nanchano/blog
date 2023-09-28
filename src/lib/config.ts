9ad1f3af38c57655edb6c773ce385654983843d3
nanchano <nicolas.anchanio@gmail.com>
2023-09-28 14:57:08 -0300

    domain

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import {dev} from '$app/environment';
 
 export const title = 'Nicolas Anchano';
 export const description = 'My personal blog';
-export const url = dev ? 'http://localhost:5173/': 'https://tbd.tbd/';
+export const url = dev ? 'http://localhost:5173/': 'https://tiburcio.vercel.app/';
