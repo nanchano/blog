894bbdd0d09fa996b2664460ed580e828ea3936f
nanchano <nicolas.anchanio@gmail.com>
2023-09-28 12:21:41 -0300

    basic contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,2 +1,14 @@
 <h1>Contact</h1>
-<p>TBD</p>
+<p>Nicolas Anchano</p>
+<div>
+	Email:
+	<a href="mailto:nicolasanchano@gmail.com">nicolasanchano@gmail.com</a>
+</div>
+<div>
+	LinkedIn:
+	<a href="https://www.linkedin.com/in/nicolas-anchano/en">Nicolas Anchano</a>
+</div>
+<div>
+	GitHub:
+	<a href="https://github.com/nanchano">nanchano</a>
+</div>
